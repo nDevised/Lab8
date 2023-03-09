a930d7e6e84209189567ff72dbfc58ed2e782dda
nDevised <90827405+nDevised@users.noreply.github.com>
2023-03-08 18:50:35 -0700

    deleteCity implemented

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -69,8 +69,19 @@ public class CustomList extends ArrayAdapter<City> {
      * delete the city object from the list
      * @throws Exception if the city is not deleted successfully (i.e it isn't in the list)
      */
-    public void deleteCity(City city) {
+    public void deleteCity(City city) throws Exception {
+        boolean isDeleted = false;
+        for(int i = 0; i < cities.size(); i++){
+            if (cities.get(i).getCityName() == city.getCityName() && cities.get(i).getProvinceName() == city.getProvinceName()){
+                cities.remove(i);
+                isDeleted = true;
+            }
+        }
 
+        // throw an exception
+        if (!isDeleted) {
+            throw new Exception("This city is not in the list, failed to delete.");
+        }
     }
 
 }
